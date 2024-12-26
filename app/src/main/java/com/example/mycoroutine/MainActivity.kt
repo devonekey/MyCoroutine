9655e840b8e7d6e62ef9555c7bcf4308c74598b3
OneKey <devonekey@gmail.com>
2024-12-26 16:07:20 +0900

    feat: MainActivity에서의 Compose 관련 코드 정리

--- app/src/main/java/com/example/mycoroutine/MainActivity.kt
+++ app/src/main/java/com/example/mycoroutine/MainActivity.kt
@@ -6,12 +6,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.mycoroutine.ui.theme.MyCoroutineTheme
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
@@ -33,10 +29,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyCoroutineTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
                 }
             }
         }
@@ -109,21 +101,5 @@ class MainActivity : ComponentActivity() {
 
     companion object {
         private val TAG: String = MainActivity::class.java.simpleName
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyCoroutineTheme {
-        Greeting("Android")
     }
 }
