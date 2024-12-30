b109840ecd0e5664dca473330dcd2a1bb84ad16b
OneKey <devonekey@gmail.com>
2024-12-30 18:18:05 +0900

    feat: Section 컴포넌트 추가

---
+++ app/src/main/java/com/example/mycoroutine/ui/component/Section.kt
@@ -0,0 +1,67 @@
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.mycoroutine.ui.component.Description
+import com.example.mycoroutine.ui.component.Title
+
+@Composable
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFFFFF
+)
+fun Section(
+    @PreviewParameter(SectionScreenParamProvider::class) param: SectionParam,
+    onClick: () -> Unit = {},
+    paddingTop: Dp = 16.dp,
+    paddingBottom: Dp = 16.dp,
+) {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .clickable { onClick() }
+            .padding(horizontal = 16.dp)
+            .padding(
+                start = 16.dp,
+                top = paddingTop,
+                end = 16.dp,
+                bottom = paddingBottom
+            )
+    ) {
+        val (titleRef, descriptionRef) = createRefs()
+
+        Title(
+            title = param.title,
+            modifier = Modifier.constrainAs(titleRef) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+            }
+        )
+        Description(
+            description = param.description,
+            modifier = Modifier.constrainAs(descriptionRef) {
+                top.linkTo(titleRef.bottom, margin = 4.dp)
+                start.linkTo(titleRef.start)
+            }
+        )
+    }
+}
+
+data class SectionParam(
+    val title: String,
+    val description: String
+)
+
+class SectionScreenParamProvider : PreviewParameterProvider<SectionParam> {
+    override val values: Sequence<SectionParam>
+        get() = listOf(SectionParam("Section", "Description")).asSequence()
+}
