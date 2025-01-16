929ac247e5a9cf7522eac813a1feca2d6f73d03d
OneKey <devonekey@gmail.com>
2025-01-17 01:43:44 +0900

    feat: async 결과를 가져오는 코드 추가

--- app/src/main/java/com/example/mycoroutine/domain/logic/logic_2_1.kt
+++ app/src/main/java/com/example/mycoroutine/domain/logic/logic_2_1.kt
@@ -2,19 +2,27 @@ package com.example.mycoroutine.domain.logic
 
 import android.util.Log
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 
 private val TAG: String = "Logic_2_1"
 
+@OptIn(InternalCoroutinesApi::class)
 fun logic_2_1() = runBlocking {
     val task = GlobalScope.async { doSomething() }
 
     task.join()
 
-    Log.d(TAG, "Complete")
+    if (task.isCancelled) {
+        val exception = task.getCancellationException()
+
+        Log.d(TAG, "Error with message: ${exception.cause}")
+    } else {
+        Log.d(TAG, "Complete")
+    }
 }
 
-fun doSomething() {
+private fun doSomething() {
     throw UnsupportedOperationException("Can't do")
 }
