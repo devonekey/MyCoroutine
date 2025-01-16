ff82d9a92e6164a7da34b42b414f26d2db9af2e3
OneKey <devonekey@gmail.com>
2025-01-16 22:24:49 +0900

    feat: 중단 가능한 연산들을 동시에 실행하는 코드 추가

--- app/src/main/java/com/example/mycoroutine/domain/logic/logic_1_5.kt
+++ app/src/main/java/com/example/mycoroutine/domain/logic/logic_1_5.kt
@@ -1,6 +1,7 @@
 package com.example.mycoroutine.domain.logic
 
 import android.util.Log
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlin.system.measureTimeMillis
@@ -14,8 +15,15 @@ fun logic_1_5() = runBlocking {
 
         Log.d(TAG, "Hello, $name $lastName")
     }
+    val time2 = measureTimeMillis {
+        val name = async { getName() }
+        val lastName = async { getLastName() }
+
+        Log.d(TAG, "Hello, ${name.await()} ${lastName.await()}")
+    }
 
     Log.d(TAG, "Execution took $time ms")
+    Log.d(TAG, "Execution took $time2 ms")
 }
 
 suspend fun getName(): String {
