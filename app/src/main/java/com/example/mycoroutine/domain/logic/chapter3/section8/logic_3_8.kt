8ae001ffe1dadd8d439b814fa4a3333cc9507c87
OneKey <devonekey@gmail.com>
2025-02-03 21:53:23 +0900

    feat: 여러 REST를 호출하는 코드 추가

---
+++ app/src/main/java/com/example/mycoroutine/domain/logic/chapter3/section8/logic_3_8.kt
@@ -0,0 +1,38 @@
+package com.example.mycoroutine.domain.logic.chapter3.section8
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.newFixedThreadPoolContext
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import javax.xml.parsers.DocumentBuilderFactory
+
+private val feeds = listOf(
+    "https://www.npr.org/rss/rss.php?id=1001",
+    "http://rss.cnn.com/rss/cnn_topstories.rss",
+//    "http://feeds.foxnews.com/foxnews/politics?format=xml"
+)
+private val dispatcher = newFixedThreadPoolContext(2, "IO")
+private val factory = DocumentBuilderFactory.newInstance()
+
+fun logic_3_8(requests: MutableList<Deferred<List<String>>>) = feeds.mapTo(requests) {
+    asyncFetchRssHeadlines(it, dispatcher)
+}
+
+private fun asyncFetchRssHeadlines(
+    feed: String,
+    dispatcher: CoroutineDispatcher
+) = GlobalScope.async(dispatcher) {
+    val builder = factory.newDocumentBuilder()
+    val xml = builder.parse(feed)
+    val news = xml.getElementsByTagName("channel").item(0)
+
+    (0 until news.childNodes.length)
+        .map { news.childNodes.item(it) }
+        .filter { Node.ELEMENT_NODE == it.nodeType }
+        .map { it as Element }
+        .filter { "item" == it.tagName }
+        .map { it.getElementsByTagName("title").item(0).textContent }
+}
