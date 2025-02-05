cb3cd4df2765bd2ea0896236fc712f223a6d82fb
OneKey <devonekey@gmail.com>
2025-02-05 21:51:39 +0900

    feat: 앞서 생성한 모델에 따라 REST API 호출 로직 개선

--- app/src/main/java/com/example/mycoroutine/domain/logic/chapter4/section1/logic_4_1.kt
+++ app/src/main/java/com/example/mycoroutine/domain/logic/chapter4/section1/logic_4_1.kt
@@ -1,6 +1,9 @@
 package com.example.mycoroutine.domain.logic.chapter4.section1
 
+import com.example.mycoroutine.domain.model.Article
+import com.example.mycoroutine.domain.model.Feed
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.newFixedThreadPoolContext
@@ -9,20 +12,24 @@ import org.w3c.dom.Node
 import javax.xml.parsers.DocumentBuilderFactory
 
 private val feeds = listOf(
-    "https://www.npr.org/rss/rss.php?id=1001",
-    "http://rss.cnn.com/rss/cnn_topstories.rss",
-    "http://feeds.foxnews.com/foxnews/politics?format=xml",
-    "htt:myNewsFeed"
+    Feed("npr", "https://www.npr.org/rss/rss.php?id=1001"),
+    Feed("cnn", "http://rss.cnn.com/rss/cnn_topstories.rss"),
+    Feed("fox", "http://feeds.foxnews.com/foxnews/politics?format=xml"),
+    Feed("inv", "htt:myNewsFeed")
 )
 private val dispatcher = newFixedThreadPoolContext(2, "IO")
 private val factory = DocumentBuilderFactory.newInstance()
 
-private fun asyncFetchRssHeadlines(
-    feed: String,
+fun logic_4_1(requests: MutableList<Deferred<List<Article>>>) = feeds.mapTo(requests) {
+    asyncFetchArticles(it, dispatcher)
+}
+
+private fun asyncFetchArticles(
+    feed: Feed,
     dispatcher: CoroutineDispatcher
 ) = GlobalScope.async(dispatcher) {
     val builder = factory.newDocumentBuilder()
-    val xml = builder.parse(feed)
+    val xml = builder.parse(feed.url)
     val news = xml.getElementsByTagName("channel").item(0)
 
     (0 until news.childNodes.length)
@@ -30,5 +37,11 @@ private fun asyncFetchRssHeadlines(
         .filter { Node.ELEMENT_NODE == it.nodeType }
         .map { it as Element }
         .filter { "item" == it.tagName }
-        .map { it.getElementsByTagName("title").item(0).textContent }
+        .map {
+            val title = it.getElementsByTagName("title").item(0).textContent
+            val summary = it.getElementsByTagName("description").item(0).textContent
+
+            Article(feed.name, title, summary)
+        }
 }
+
